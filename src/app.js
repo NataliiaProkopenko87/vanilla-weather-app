9715df1c4f1739cf684a2c37bfb898ca8ef0f93b
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-05-20 12:43:16 +0200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-alert("Hello world");
+function formatDate(timestamp) {
+  //calculate date
+}
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = "Friday 5:00";
+}
+
+let apiKey = "d2c2ea35d1cdabb51672d9219b227afa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
