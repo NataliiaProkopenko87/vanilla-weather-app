a069438e8d870164cc1a88edb4104cc4deedac60
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-05-22 12:46:07 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,7 +43,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d2c2ea35d1cdabb51672d9219b227afa";
-let city = "Wroclaw";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "d2c2ea35d1cdabb51672d9219b227afa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
