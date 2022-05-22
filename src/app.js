f9f8661f67e62df9393f76ac5ec5e620847634db
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-05-22 11:17:52 +0200

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 function formatDate(timestamp) {
   //calculate date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    return `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    return `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 function displayTemperature(response) {
   console.log(response.data);
@@ -14,7 +35,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = "Friday 5:00";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "d2c2ea35d1cdabb51672d9219b227afa";
