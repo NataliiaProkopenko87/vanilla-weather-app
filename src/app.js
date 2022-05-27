14164ae4f7283291ee92ef529281baddcd6d3cbb
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-05-27 21:50:34 +0200

    Remove the imperial conversation

--- src/app.js
+++ src/app.js
@@ -119,8 +119,8 @@ function displayFahrenheitTemperature(event) {
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
+  /* fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");*/
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
@@ -130,9 +130,10 @@ let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+/*let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);*/
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 search("New York");
